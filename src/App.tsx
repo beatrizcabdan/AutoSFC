f519631ffb57c7a33bdbce958f8691adad2d2f92
beacab <beatriz.cabrero.daniel@gmail.com>
2024-11-27 15:17:46 +0100

    Added chart2, removed faker.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,65 @@
 import { useState } from 'react'
 import './App.css'
 
+import React from 'react';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Filler,
+  Legend,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Filler,
+  Legend
+);
+
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top' as const,
+    },
+    title: {
+      display: true,
+      text: 'Chart.js Line Chart',
+    },
+  },
+};
+
+const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      fill: true,
+      label: 'Dataset 2',
+      data: labels.map(() => [1,2,3,4,5]),
+      borderColor: 'rgb(53, 162, 235)',
+      backgroundColor: 'rgba(53, 162, 235, 0.5)',
+    },
+  ],
+};
+
 function App() {
   return (
       <>
+          <div>
+              <Line options={options} data={data} />
+          </div>
           <div>
               Original signals plot
           </div>
