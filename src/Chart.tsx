798a7906ff2b037356774bb20ae0ebf820961b3c
Anton J Olsson <anton.j.olsson@bredband.net>
2024-12-11 00:00:00 +0100

    feat: 🎸 basic morton encoding

--- src/Chart.tsx
+++ src/Chart.tsx
@@ -1,14 +1,26 @@
 import React, {useEffect, useRef} from "react";
-import {ZCurve} from "@thi.ng/morton";
 
 function mortonEncode2D(xData: number[], yData: number[]) {
-    // noinspection TypeScriptValidateTypes
-    const z = new ZCurve(2, 32);
-    return xData.map((x, i) => {
-        const m = z.encode([x, yData[i]])
-        // console.log(m.toString())
-        return m
+    const resultArr: number[] = []
+    xData.forEach((x, i) => {
+        let xn = BigInt(x)
+        xn = (xn | (xn << 16n)) & 0x0000FFFF0000FFFFn
+        xn = (xn | (xn << 8n)) & 0x00FF00FF00FF00FFn
+        xn = (xn | (xn << 4n)) & 0x0F0F0F0F0F0F0F0Fn
+        xn = (xn | (xn << 2n)) & 0x3333333333333333n
+        xn = (xn | (xn << 1n)) & 0x5555555555555555n
+
+        let yn = BigInt(Math.round(yData[i] * 10000))
+        yn = (yn | (yn << 16n)) & 0x0000FFFF0000FFFFn
+        yn = (yn | (yn << 8n)) & 0x00FF00FF00FF00FFn
+        yn = (yn | (yn << 4n)) & 0x0F0F0F0F0F0F0F0Fn
+        yn = (yn | (yn << 2n)) & 0x3333333333333333n
+        yn = (yn | (yn << 1n)) & 0x5555555555555555n
+
+        const result = xn | (yn << 1n)
+        resultArr.push(Number(result))
     })
+    return resultArr
 }
 
 export function Chart(props: { name: string, data: number[], type: string, xAxisName: string, yAxisName: string }) {
@@ -33,6 +45,10 @@ export function Chart(props: { name: string, data: number[], type: string, xAxis
         ctx.stroke()
     }
 
+    function getX(i: number, canvas: HTMLCanvasElement, padding: number) {
+        return (i / props.data.length) * (canvas.width - padding * 2) + padding;
+    }
+
     useEffect(() => {
         if (props.data && canvasRef.current) {
             const sortedData = [...props.data].sort()
@@ -47,9 +63,9 @@ export function Chart(props: { name: string, data: number[], type: string, xAxis
 
                 ctx.strokeStyle = "blue"
                 ctx.beginPath()
-                ctx.lineWidth = 1.5
+                ctx.lineWidth = 3
                 props.data.forEach((point, i) => {
-                    const x = (i / props.data.length) * (canvas.width - padding * 2) + padding
+                    const x = getX(i, canvas, padding)
                     const y = (canvas.height - padding * 2) * (point - minSpeed) / (maxSpeed - minSpeed) + padding
                     if (i === 0) {
                         ctx.moveTo(x, y)
@@ -60,17 +76,32 @@ export function Chart(props: { name: string, data: number[], type: string, xAxis
                 ctx.stroke()
             } else {
                 // Morton scatterplot
-                const xData = [...Array(props.data.length).keys()]
-                const data = mortonEncode2D(xData, props.data)
+                const timeSteps = [...Array(props.data.length).keys()]
+                const mortonData = mortonEncode2D(timeSteps, props.data).map(m => Number(m.toString()))
+                console.log(mortonData)
+                const mortonSorted = [...mortonData].sort((a, b) => a - b)
+                const minMorton = mortonSorted[0] /*30*/
+                const maxMorton = mortonSorted[mortonSorted.length - 1] /*86000*/
+
+                mortonData.forEach((m, i) => {
+                    const x = getX(i, canvas, padding)
+                    const y = (canvas.width - padding * 2) * (m - minMorton) / (maxMorton - minMorton) + padding
+
+                    ctx.beginPath();
+                    ctx.lineWidth = 1
+                    // noinspection JSSuspiciousNameCombination
+                    ctx.arc(y, x, 1, 0, 2 * Math.PI);
+                    ctx.stroke();
+                })
             }
         }
-    }, [props.data, canvasRef]);
+    }, [props.data, canvasRef.current]);
 
     useEffect(() => {
         if (canvasRef.current) {
             const canvas: HTMLCanvasElement = canvasRef.current!
-            canvas.width = Number(getComputedStyle(canvas).width.replace('px', ''))
-            canvas.height = Number(getComputedStyle(canvas).height.replace('px', ''))
+            canvas.width = Number(getComputedStyle(canvas).width.replace('px', '') * 2)
+            canvas.height = Number(getComputedStyle(canvas).height.replace('px', '') * 2)
             ctx = canvas.getContext('2d')
             drawAxes(canvas);
         }
