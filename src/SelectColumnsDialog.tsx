ca3346f3b93284af12451495b7a0f7d8d9ca21a9
Anton J Olsson <anton.j.olsson@bredband.net>
2025-01-17 00:00:00 +0100

    feat: 🎸 add dialog buttons

--- src/SelectColumnsDialog.tsx
+++ src/SelectColumnsDialog.tsx
@@ -5,10 +5,22 @@ export function SelectColumnsDialog(props: {show: boolean, dataLabelsRef:  Mutab
     console.log(props.dataLabelsRef.current)
     return <div className={'light-box'}>
             <dialog open className={'dialog'}>
-                <p>Select displayed data</p>
+                <h2>Select displayed data</h2>
                 <form method="dialog">
-                    {props.dataLabelsRef.current.map(label => )}
-                    <button>OK</button>
+                    <div className={'checkbox-list'}>
+                    {props.dataLabelsRef.current.map((label, i) => {
+                        const id = `checkbox${String(i)}`
+                        // noinspection HtmlUnknownAttribute
+                        return <div>
+                            <input type="checkbox" name="state_name" value="Connecticut" id={id}/>
+                            <label for={id}>{label}</label>
+                        </div>
+                    })}
+                    </div>
+                    <div className={'buttons'}>
+                        <button className={'ok-button button'}>OK</button>
+                        <button className={'button'}>Cancel</button>
+                    </div>
                 </form>
             </dialog>;
     </div>
