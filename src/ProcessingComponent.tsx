4832310a3d1182ea8a2acbde249ecbf0cc01460f
Anton J Olsson <anton.j.olsson@bredband.net>
2025-03-26 00:00:00 +0100

    style: 💄 make transform controls grid!!!

--- src/ProcessingComponent.tsx
+++ src/ProcessingComponent.tsx
@@ -5,25 +5,28 @@ export function ProcessingComponent(props: { displayedDataLabels: string[] | nul
     onScalesChanged: (index: number, scale: (number | undefined)) => void, onOffsetsChanged: (index: number, offset: number) => void
 }) {
     return <div className={'control-container'} id={'process-container'}>
-        <h3>Scale/transform</h3>
-        <div className={'signal-rows'}>
+        <h3>Transform</h3>
+        <div className={'signals-grid'}>
+            <span className={'input-label signal-label'}>Signal</span>
+            <span className={'input-label offset-label'}>Offset</span>
+            <span className={'input-label scale-label'}>Scale</span>
             {props.displayedDataLabels?.map((signal, i) =>
-                <div className={'signal-row'} key={i}>
-                    <span style={{background: props.lineColors[i]}} className={'color-line'}></span>
-                    <span className={'signal-name'}>{signal}</span>
-                    <label className={'input-label'}>
-                        Offset
+                <>
+                    <div className={'signal-cell'} key={i}>
+                        <span style={{background: props.lineColors[i]}} className={'color-line'}></span>
+                        <span className={'signal-name'}>{signal}</span>
+                    </div>
+                    <label className={'input-label offset-label'}>
                         <input type="number" value={0}
                                onChange={(e) => props.onOffsetsChanged(i, Number(e.target.value))}/>
                     </label>
-                    <label className={'input-label'}>
-                        Scale
+                    <label className={'input-label scale-label'}>
                         <input type="number" value={props.scales[i]} onBlur={() =>
                             props.onScalesChanged(i, Number(props.scales[i] ?? DEFAULT_SCALING_FACTOR))}
                                onChange={(e) =>
                                    props.onScalesChanged(i, e.target.value ? Number(e.target.value) : undefined)}/>
                     </label>
-                </div>
+                </>
             )}
         </div>
     </div>;
