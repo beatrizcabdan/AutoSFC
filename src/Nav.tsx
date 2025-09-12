b30cf6e958e448eed340ef34ad24e674cfc18b95
Anton J Olsson <anton.j.olsson@bredband.net>
2025-09-13 00:00:00 +0200

    feat: 🎸 add nav icons

--- src/Nav.tsx
+++ src/Nav.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import './Nav.scss'
-import ArrowUpwardIcon from "@mui/icons-material/ArrowUpward";
 
 export function Nav() {
     function scrollToSection(section: string) {
@@ -15,10 +14,15 @@ export function Nav() {
 
     return <div className="topnav">
         <div className="active" onClick={() => scrollToSection("#encoding-demo-div")}>
-            <ArrowUpwardIcon sx={{ mr: 1 }} />Encoding Demo</div>
-        <div onClick={() => scrollToSection("#comp-demo-div")}>Comparison Demo</div>
-        <div onClick={() => scrollToSection("#work")}>Previous work</div>
-        <div onClick={() => scrollToSection("#about")}>About SFCs</div>
-        <div onClick={() => scrollToSection("#contact")}>Contact</div>
+            <span className="material-symbols-outlined">swap_horiz</span>Encoding Demo</div>
+        <div onClick={() => scrollToSection("#comp-demo-div")}>
+            <span className="material-symbols-outlined">barcode</span>Comparison Demo
+        </div>
+        <div onClick={() => scrollToSection("#work")}>
+            <span className="material-symbols-outlined">article</span>Previous work</div>
+        <div onClick={() => scrollToSection("#about")}>
+            <span className="material-symbols-outlined">info</span>About SFCs</div>
+        <div onClick={() => scrollToSection("#contact")}>
+            <span className="material-symbols-outlined">alternate_email</span>Contact</div>
     </div>
 }
