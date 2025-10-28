1bef7784b7f0e1cfb5a866c0da8a751995baf44d
Anton J Olsson <ao@antolsson.se>
2025-10-29 00:00:00 +0100

    ci: 🎡 set vite base based on branch name

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import * as child_process from "node:child_process";
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: "/",
-  plugins: [react()],
-})
+export default () => {
+  const branchName = child_process.execSync('git rev-parse --abbrev-ref HEAD').toString().trimEnd();
+  return {
+    base: `${branchName === 'main' ? '/' : '/dev'}`,
+    plugins: [react()]
+  }
+}
